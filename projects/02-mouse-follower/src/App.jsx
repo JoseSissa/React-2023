6ee5eed77ec964edcf5b2c4c28a8dd447e9cf864
Jose <sissamendoza@outlook.com>
2023-07-24 14:54:13 -0500

    mouse-follower

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0});
+
+  useEffect(() => {
+    const handleMove = (e) => {
+      const { clientX, clientY } = e
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if(enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+    // El useEffect necesita limpiar la suscripciones ya que no lo hace de forma automática
+    // Este return se ejecutará cada vez que el componente se desmonte o cuando el array de dependencias cambie, antes de ejecutar de nuevo el efecto
+    // Así nos asegurams que empieze de cero las suscripciones cada vez que se ejecuta el efecto
+    // cleanup:
+    // -> cuando el componente se desmonta
+    // -> cuando cambian las dependencias, antes de ejecutar
+    //    el efecto de nuevo
+    return () => { // cleanup method
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled]);
+
+  const handleClick = () => {
+    setEnabled(!enabled)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+      <button onClick={handleClick}>
+        {enabled ? 'Desactivar' : 'Activar'}
+      </button>
     </>
   )
 }
