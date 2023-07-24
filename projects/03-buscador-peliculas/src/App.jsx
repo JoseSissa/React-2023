8630f9bb428559a20056ae39b206ff6ba6acff3a
Jose <sissamendoza@outlook.com>
2023-07-24 15:37:05 -0500

    3er proyecto

---
+++ projects/03-buscador-peliculas/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Buscador de Películas</h1>
+        <form action="" className="form">
+          <input type="text" placeholder="Avengers, Star Wars ..."/>
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default App
